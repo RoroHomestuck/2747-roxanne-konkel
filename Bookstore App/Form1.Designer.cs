91d74970d886fa136fac93fe695cf2794e60b3bf
RoroHomestuck <rorohomestuck@gmail.com>
2024-09-13 16:26:07 -0500

    Part Developed

--- Bookstore App/Form1.Designer.cs
+++ Bookstore App/Form1.Designer.cs
@@ -28,18 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(332, 426);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlDark;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(332, 239);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 40F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(3, 242);
+            label1.Name = "label1";
+            label1.Size = new Size(254, 72);
+            label1.TabIndex = 1;
+            label1.Text = "Products";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(20, 314);
+            label2.Name = "label2";
+            label2.Size = new Size(292, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Manage the catalog of products";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Location = new Point(376, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(332, 426);
+            panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(20, 314);
+            label3.Name = "label3";
+            label3.Size = new Size(263, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Manage the list of customers";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 40F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(3, 242);
+            label4.Name = "label4";
+            label4.Size = new Size(298, 72);
+            label4.TabIndex = 1;
+            label4.Text = "Customers";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.ControlDark;
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(332, 239);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(720, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Bookstore Manager";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox2;
     }
 }
